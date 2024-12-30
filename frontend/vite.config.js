34ae39d509802f9490185fcd594524cffc539413
keinomura <kein312@gmail.com>
2024-12-30 13:48:24 +0900

    vite環境変数の読み込みBaseでできた。

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// 環境変数を読み込む
-const base = import.meta.env.VITE_BASE_URL || '/';
+export default defineConfig(({ mode }) => {
+  // 環境変数を読み込む
+  const env = loadEnv(mode, process.cwd())
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-  base: base,
+    base: env.VITE_BASE_URL,
+  }
 })
